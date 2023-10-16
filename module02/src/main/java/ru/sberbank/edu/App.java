55b81e91913a5b60358bab6b067178d4eb7187a8
YugovGrigoriy <147773450+YugovGrigoriy@users.noreply.github.com>
2023-10-17 02:09:25 +0300

    Revert "Developer"

--- module02/src/main/java/ru/sberbank/edu/App.java
+++ module02/src/main/java/ru/sberbank/edu/App.java
@@ -1,11 +1,13 @@
 package ru.sberbank.edu;
 
-import ru.sberbank.edu.iofile.TaskCompletion;
-
-public class App {
-
-    public static void main(String[] args) {
-        TaskCompletion homeWork2 = new TaskCompletion();
-
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
     }
 }
